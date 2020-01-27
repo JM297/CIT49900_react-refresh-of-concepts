0065538422e543b1a5fb5a74d5e2b172c73a6ae3
JM297 <sirweegee777@gmail.com>
2020-01-26 22:56:08 -0500

    Nav Update
    Removed the list styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom";
 export default function Nav(){
     return(
         <div  className="nav" style={{marginBottom: 25}}>
-            <ul style={{display:'contents'}} className="navBar">
+            <ul style={{display:'contents', listStyle: 'none'}} className="navBar">
                 <li><NavLink to={"/"}>Home</NavLink></li>
                 <li><NavLink to={"/list"}>Items</NavLink></li>
             </ul>
